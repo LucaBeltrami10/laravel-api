383951d7cd933c49406c920936a549759115bb4a
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-20 15:46:23 +0100

    created method index in Admin/ProjectController

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     //
+
+    public function index()
+    {
+
+        $projects = Project::all();
+
+        return response()->json(
+            $projects
+        );
+    }
 }
